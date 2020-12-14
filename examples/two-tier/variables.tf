eb2e7100755d48a71e988ed74b72a786b4046abe
Tim Davis <tim.davis.instinct@gmail.com>
2020-12-14 16:21:49 -0600

    Update variables.tf

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -1,13 +1,3 @@
-variable "public_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
-
-Example: ~/.ssh/terraform.pub
-DESCRIPTION
-}
-
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-west-2"
