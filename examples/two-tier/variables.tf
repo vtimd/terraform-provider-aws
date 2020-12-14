5eafe36c15e327a8afce8f021cbbef680a1745b1
Tim Davis <tim.davis.instinct@gmail.com>
2020-12-14 16:21:31 -0600

    Update variables.tf

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -8,10 +8,6 @@ Example: ~/.ssh/terraform.pub
 DESCRIPTION
 }
 
-variable "key_name" {
-  description = "Desired name of AWS key pair"
-}
-
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-west-2"
