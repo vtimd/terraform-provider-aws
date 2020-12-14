eb24f8de1b9a9f6577ec0befb0dd91c0e5efcdf6
Tim Davis <tim.davis.instinct@gmail.com>
2020-12-14 16:21:07 -0600

    Update main.tf

--- examples/two-tier/main.tf
+++ examples/two-tier/main.tf
@@ -100,11 +100,6 @@ resource "aws_elb" "web" {
   }
 }
 
-resource "aws_key_pair" "auth" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
 resource "aws_instance" "web" {
   # The connection block tells our provisioner how to
   # communicate with the resource (instance)
