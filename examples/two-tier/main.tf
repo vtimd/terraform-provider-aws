de918ca4836fe334c510d360f3c6af41f843c645
Tim Davis <tim.davis.instinct@gmail.com>
2021-06-06 12:24:58 -0500

    Update main.tf

--- examples/two-tier/main.tf
+++ examples/two-tier/main.tf
@@ -3,12 +3,12 @@
 #}
 
 terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "tim-demos"
-    workspaces {
-      name = "terraform-provider-aws"
-    }
+  required_version = ">= 0.12"
+  backend "s3" {
+    encrypted = true
+    bucket = "bucket-tfstate"
+    key = "prod/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
